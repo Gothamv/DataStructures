a4158e06890d34b4a566c921c9e10d9a90f82d9c
Gowtham Venkatesan <gowthamv@icloud.com>
2020-02-08 13:51:30 +0000

    element swapping in linked list

---
+++ Linked Lists/elementSwapping.py
@@ -0,0 +1,42 @@
+from singlyLinkedList import Node, LinkedList
+def nodeSwap(self, key1, key2):
+	if key1 == key2:
+		return
+	
+	prev1 = None
+	curr1 = self.head
+	while curr1 and curr1.data != key1:
+		prev1 = curr1
+		curr1 = curr1.next
+	
+	prev2 = None
+	curr2 = self.head
+	while curr2 and curr2.data != key2:
+		prev2 = curr2
+		curr2 = curr2.next
+
+	if not curr1 or not curr2:
+		return
+	
+	if prev1:
+		prev1.next = curr2
+	else:
+		self.head = curr2
+	
+	if prev2:
+		prev2.next = curr1
+	else:
+		self.head = curr1
+	
+	curr1.next, curr2.next = curr2.next, curr1.next
+
+''' Driver Code
+LinkedList.nodeSwap = nodeSwap
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+llist.nodeSwap("A","B")
+print(llist.show())
+'''
--- Linked Lists/singlyLinkedList.py
+++ Linked Lists/singlyLinkedList.py
@@ -29,6 +29,48 @@ class LinkedList:
 		newNode.next = prevNode.next
 		prevNode.next = newNode 
 
+	def deleteByValue(self, key):
+		curr = self.head
+		# Node to be deleted is a head
+		if curr and curr.data == key:
+			self.head = curr.next
+			curr = None
+			return
+		
+		#Node to be deleted is not a head
+		prev = None
+		while curr and curr.data != key:
+			prev = curr
+			curr = curr.next
+		
+		#Checking if thw while loop terminated because of None and not because of key matching
+		if curr is None:
+			return
+		prev.next = curr.next
+		curr = None
+
+	def deleteByPosition(self, pos):
+		if self.head:
+			curr = self.head
+			if pos == 0:
+				self.head = curr.next
+				curr = None
+				return
+		
+		prev = None
+		count = 0
+		if curr and count != pos:
+			prev = curr
+			curr = curr.next
+			count += 1
+
+		if curr is None:
+			return
+		
+		prev.next = curr.next
+		curr = None
+		return
+
 	def show(self) -> list:
 		showList = []
 		currNode = self.head
@@ -36,13 +78,27 @@ class LinkedList:
 			showList.append(currNode.data)
 			currNode = currNode.next	
 		return showList
-	
+
+	def lenIterative(self) -> int:
+		count = 0
+		curr = self.head
+		while curr:
+			count +=1
+			curr = curr.next
+		return count
+
+	def lenRecursive(self, node):
+		if node is None:
+			return 0
+		return 1 + self.lenRecursive(node.next)
+
+''' Driver Code
 llist = LinkedList()
 llist.append("A")
 llist.append("B")
 llist.append("C")
 llist.append("D")
+llist.deleteByValue("B")
+llist.deleteByPosition(0)
 print(llist.show())
-
-
-
+'''
